20793092ebc84cb1b2cf72cb9906f2850ee3d3b6
Nhan <nguyenngocbaonhan2003@gmail.com>
2024-02-02 16:49:12 +0700

    update base

---
+++ ReadingComicWebSite/src/main/java/com/example/readingcomicwebsite/ReadingComicWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.readingcomicwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingComicWebSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReadingComicWebSiteApplication.class, args);
+    }
+
+}
---
+++ ReadingComicWebSite/src/test/java/com/example/readingcomicwebsite/ReadingComicWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readingcomicwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingComicWebSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
